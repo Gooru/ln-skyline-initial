4dc9e9faa035ba8bb580103b50c85bf7178b1d79
am-gooru <ashish@gooru.org>
2019-02-08 11:28:46 +0530

    Remove hardcoded source with generated one

--- src/main/java/org/gooru/skylineinitial/infra/services/learnerprofile/LearnerProfilePersisterDao.java
+++ src/main/java/org/gooru/skylineinitial/infra/services/learnerprofile/LearnerProfilePersisterDao.java
@@ -30,9 +30,9 @@ interface LearnerProfilePersisterDao {
       "INSERT INTO learner_profile_competency_evidence(user_id, gut_code, class_id, course_id, unit_id, lesson_id, latest_session_id,"
           + " collection_id, collection_path_id, collection_score, collection_type, content_source, created_at, updated_at) "
           + " VALUES (:userId, :competencies, :classId, null, null, null, :latestSessionId, :collectionId, null, :collectionScore, 'assessment', "
-          + " 'diagnostic', now(), now()) ON CONFLICT (user_id, gut_code, collection_id) DO UPDATE SET latest_session_id = :latestSessionId, "
+          + " :profileSource, now(), now()) ON CONFLICT (user_id, gut_code, collection_id) DO UPDATE SET latest_session_id = :latestSessionId, "
           + " class_id = :classId, course_id = null, unit_id = null, lesson_id = null, collection_path_id = null, collection_score ="
-          + " :collectionScore, content_source = 'diagnostic', updated_at = now()")
+          + " :collectionScore, content_source = :profileSource, updated_at = now()")
   void persistLearnerProfileCompetencyEvidence(@BindBean LearnerProfilePersisterModel model,
       @Bind("competencies") List<String> competencies);
 
@@ -41,9 +41,9 @@ interface LearnerProfilePersisterDao {
       "INSERT INTO learner_profile_competency_evidence_ts(user_id, gut_code, class_id, course_id, unit_id, lesson_id, latest_session_id,"
           + " collection_id, collection_path_id, collection_score, collection_type, content_source, status, created_at, updated_at) "
           + " VALUES (:userId, :competencies, :classId, null, null, null, :latestSessionId, :collectionId, null, :collectionScore, "
-          + " 'assessment', 'diagnostic', :status, now(), now()) ON CONFLICT (user_id, gut_code, collection_id, status) "
+          + " 'assessment', :profileSource, :status, now(), now()) ON CONFLICT (user_id, gut_code, collection_id, status) "
           + " DO UPDATE SET latest_session_id = :latestSessionId, collection_path_id = null, collection_score = :collectionScore, "
-          + " content_source = 'diagnostic', updated_at = now()")
+          + " content_source = :profileSource, updated_at = now()")
   void persistLearnerProfileCompetencyEvidenceTS(@BindBean LearnerProfilePersisterModel model,
       @Bind("competencies") List<String> competencies);
 
